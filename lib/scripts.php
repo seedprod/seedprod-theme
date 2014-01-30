0d902a67d4077836c07d6ebb0a9397f1518d8778
John Turner <johnnytee@gmail.com>
2014-01-29 20:14:03 -0500

    tweaks

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'bac6d64efc7cee528e789e1a01b9b497');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9dc030ba072454bb728053fea9d21703');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
